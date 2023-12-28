7ecd703561fea8e3a992c39c2ccc6450c132ee4c
Etienne de Dianous <etiennededi@hotmail.fr>
2023-12-28 12:18:18 +0100

    ok

--- app/javascript/controllers/airs_controller.js
+++ app/javascript/controllers/airs_controller.js
@@ -19,7 +19,6 @@ export default class extends Controller {
     const wrapper = event.target.closest('.nested-fields')
     if (wrapper.dataset.newRecord == 'true') {
       wrapper.remove()
-    // } else {
       wrapper.querySelector("input[name*='_destroy']").value = '1'
       wrapper.style.display = 'none'
     }
