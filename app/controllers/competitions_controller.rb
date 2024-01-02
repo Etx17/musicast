dd111f38441704721f91ff2cfb949a1e6433f259
Etienne de Dianous <etiennededi@hotmail.fr>
2024-01-02 13:10:04 +0100

    ok

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -26,10 +26,6 @@ class CompetitionsController < ApplicationController
   def edit
     @organism = Organism.find(params[:organism_id])
     @competition = Competition.find(params[:id])
-
-    unless request.xhr?
-      redirect_to organisateur_dashboard_path # replace with the path of your main page
-    end
   end
 
   # POST /competitions or /competitions.json
