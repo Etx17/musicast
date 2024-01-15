1a7a22af78324237fab9dbd5654801d04d2ab3b8
Etienne de Dianous <etiennededi@hotmail.fr>
2024-01-15 13:26:08 +0100

    i can add 2 pauses but they have to be added in order

--- app/javascript/controllers/start_time_controller.js
+++ app/javascript/controllers/start_time_controller.js
@@ -10,6 +10,12 @@ export default class extends Controller {
     var day = event.currentTarget.dataset.day;
 
     var startTimes = Array.from(document.querySelectorAll(`[id^="start_time_${day}"]`)).map(el => el.value);
+    var pauseTimes = Array.from(document.querySelectorAll(`[id^="pause_time_${day}"]`)).map(el => el.textContent.split(' - ')[0]);
+
+    console.log(pauseTimes, "pausetimes");
+    var latestPauseTime = pauseTimes.sort().pop();
+
+    startTimes = startTimes.filter(time => time > latestPauseTime);
 
     document.querySelector('#pause_start_time').innerHTML = startTimes.map(time => `<option value="${time}">${time}</option>`).join("\n");
   }
