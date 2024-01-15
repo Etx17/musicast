5e53c129167cc3c520ea801d2f21d69a6779f160
Etienne de Dianous <etiennededi@hotmail.fr>
2024-01-15 15:18:48 +0100

    ok i can add a pause on a single day when i have multiple days

--- app/javascript/controllers/start_time_controller.js
+++ app/javascript/controllers/start_time_controller.js
@@ -8,11 +8,10 @@ export default class extends Controller {
   fetchStartTimes(event) {
     console.log("fecthing start times");
     var day = event.currentTarget.dataset.day;
-
+    console.log(day)
     var startTimes = Array.from(document.querySelectorAll(`[id^="start_time_${day}"]`)).map(el => el.value);
     var pauseTimes = Array.from(document.querySelectorAll(`[id^="pause_time_${day}"]`)).map(el => el.textContent.split(' - ')[0]);
 
-    console.log(pauseTimes, "pausetimes");
     var latestPauseTime = pauseTimes.sort().pop();
 
     if(!latestPauseTime) {
@@ -21,5 +20,12 @@ export default class extends Controller {
     startTimes = startTimes.filter(time => time > latestPauseTime);
 
     document.querySelector('#pause_start_time').innerHTML = startTimes.map(time => `<option value="${time}">${time}</option>`).join("\n");
+
+    const form = document.querySelector("#pauseModal form");
+    const dateInput = form.querySelector(".pause_day");
+    if (dateInput) {
+      dateInput.value = day;
+    }
+    console.log(dateInput.value, "dateinput value");
   }
 }
