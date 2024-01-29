992db26f6faf6637f87002a938fce0b7d92c927b
Etienne de Dianous <etiennededi@hotmail.fr>
2024-01-29 21:45:17 +0100

    extract pdf ok

--- app/models/inscription_item_requirement.rb
+++ app/models/inscription_item_requirement.rb
@@ -3,5 +3,30 @@ class InscriptionItemRequirement < ApplicationRecord
   belongs_to :requirement_item
 
   has_one_attached :submitted_file
-  
+
+
+  def extract_pdf_content
+    if requirement_item.is_pdf?
+      content = submitted_file.download
+      reader = PDF::Reader.new(StringIO.new(content))
+      text = reader.pages.map(&:text).join(" ").gsub(/\s+/, ' ')
+      text[0..500]
+    end
+  end
+
+  def send_to_openai
+    text = extract_pdf_content
+    api_key = Rails.application.credentials.dig(:open_ai, :test_key)
+    if text
+      OpenAI::API.new.create_completion(
+        engine: "text-davinci-002",
+        prompt: "Tu ne répond que par '0'( = non), '1' (= oui), ou '2' (= je sais pas). Voici le début d'un document. Répond simplement '1' si le document te semble être un #{requirement_item.item_type} , '0' si ca n'est pas le cas, '2' si tu ne sais pas. Tu ne réponds pas par une phrase, seulement un numéro.Voici l'extrait: " + text,
+        max_tokens: 200,
+        headers: {
+          'Authorization' => "Bearer #{Rails.application.credentials.dig(:open_ai, :test_key)}"
+        }
+      )
+    end
+  end
+
 end
--- app/models/requirement_item.rb
+++ app/models/requirement_item.rb
@@ -3,14 +3,14 @@ class RequirementItem < ApplicationRecord
 
   has_many :inscription_item_requirements, dependent: :destroy
 
-  enum type_item: { youtube_link: 0, recommendation_letter_pdf: 1, parental_authorization_pdf: 2, motivation_essay: 3 }
+  enum type_item: { youtube_link: 0, recommendation_letter: 1, parental_authorization: 2, motivation_essay: 3 }
 
   def self.text_items
     %w[youtube_link motivation_essay]
   end
 
   def self.pdf_items
-    %w[recommendation_letter_pdf parental_authorization_pdf]
+    %w[recommendation_letter parental_authorization]
   end
 
   def is_pdf?
