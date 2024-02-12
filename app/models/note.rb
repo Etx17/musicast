6020e8da497c7adde69f347c2c96803bd1e88a48
Etienne de Dianous <etiennededi@hotmail.fr>
2024-02-12 12:14:35 +0100

    ok

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,7 @@
 class Note < ApplicationRecord
   belongs_to :jury
   belongs_to :inscription
+
+  validates :note_value, presence: true
+  validates :note_value, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
 end
