6a850439d102c6d7ba1f23d0e171121250f0d2ce
Etienne de Dianous <etiennededi@hotmail.fr>
2024-03-08 15:26:47 +0100

    #next_tour simple test

--- spec/models/tour_spec.rb
+++ spec/models/tour_spec.rb
@@ -6,4 +6,14 @@ RSpec.describe Tour, type: :model do
       expect(create(:tour)).to be_valid
     end
   end
+
+  describe "#next_tour" do
+    let(:category) { create(:category) }
+    it "returns the correct next tour" do
+      tour = category.tours.first
+      tour.update(is_finished: true)
+      tour2 = category.tours.first(2).last
+      expect(tour.next_tour).to eq(tour2)
+    end
+  end
 end
