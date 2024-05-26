54f1b3e9cf65fba7b53d20a86dac802c1d69b971
etienne de dianous <etiennededi@hotmail.fr>
2024-05-26 17:27:00 +0200

    improved search functionallity but adde complexity with country code, need to fix that later for now not a big deal as long as it works

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,26 +15,33 @@ class PagesController < ApplicationController
 
 
   def home
+    # change the params country "France" to it's short country code equivalent if there is a params country. Then make it session
+    if params[:country].present?
+      country = ISO3166::Country.all.find { |c| c.iso_short_name == params[:country] }
+      country_code_params = country&.alpha2
+      session[:country] = country_code_params if country_code_params.present?
+    end
+
     session[:discipline] = params[:discipline] if params[:discipline].present?
-    session[:country] = params[:country][:country] if params[:country].present? && params[:country][:country].present?
+    # session[:country] = params[:country][:country] if params[:country].present? && params[:country][:country].present?
 
     @disciplines = MusicCategories::DISCIPLINES
-
-    # @edition_competitions = EditionCompetition.includes(:categories, :address)
-    #                                       .where('end_of_registration > ?', Time.now)
-    #                                       .where(status: 'published')
-    #                                     .order(:end_of_registration)
     @edition_competitions = EditionCompetition.published_and_upcoming
 
     if session[:discipline].present?
-      @edition_competitions = @edition_competitions.joins(:categories)
-                                                    .where(categories: { discipline: session[:discipline].to_i }).explain(analyze: true)
-    end # This means that only records with matching values in both tables will be included in the result set.
+      @edition_competitions = @edition_competitions.joins(:categories).where(categories: { discipline: session[:discipline].to_i })
+    end
 
     if session[:country].present?
       country = ISO3166::Country[session[:country]].iso_short_name
       @edition_competitions = @edition_competitions.joins(:address)
                                                     .where(addresses: { country: country })
     end
+
+
+    @countries_with_upcoming_competitions = EditionCompetition.includes(:address)
+      .where('edition_competitions.start_date > ?', Time.now)
+      .pluck('addresses.country')
+      .uniq
   end
 end
