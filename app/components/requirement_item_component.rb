9085f972c2a61acb96b2cb97f402b833401935ef
etienne de dianous <etiennededi@hotmail.fr>
2024-07-19 15:32:06 +0200

    compoennt for requirement item

---
+++ app/components/requirement_item_component.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class RequirementItemComponent < ViewComponent::Base
+  def initialize(requirement_item:)
+    @requirement_item = requirement_item
+  end
+
+end
--- app/components/semi_imposed_work_card_component.rb
+++ app/components/semi_imposed_work_card_component.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class SemiImposedWorkCardComponent < ViewComponent::Base
+  with_collection_parameter :semi_imposed_work
+
   def initialize(semi_imposed_work:)
     @semi_imposed_work = semi_imposed_work
   end
