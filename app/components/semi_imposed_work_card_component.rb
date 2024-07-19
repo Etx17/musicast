41330d028b326a42bcc48046075a9b9e9da05a76
etienne de dianous <etiennededi@hotmail.fr>
2024-07-19 11:22:23 +0200

    semi imposd work card component

---
+++ app/components/semi_imposed_work_card_component.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class SemiImposedWorkCardComponent < ViewComponent::Base
+  def initialize(semi_imposed_work:)
+    @semi_imposed_work = semi_imposed_work
+  end
+
+end
