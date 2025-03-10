a0473c846aa9407df88b808b7e31ebddea7939d8
Etienne de Dianous <etiennededi@hotmail.fr>
2025-03-10 21:38:38 +0100

    ok

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,9 +4,9 @@ class Address < ApplicationRecord
   VALID_ZIPCODE_REGEX = /\A[0-9a-zA-Z]{5}\z/
   VALID_ADDRESS_REGEX = /\A[a-zA-Z0-9\s,.'-]*\z/
 
-  validates :line1, presence: true, format: { with: VALID_ADDRESS_REGEX }
-  validates :city, presence: true, format: { with: VALID_ADDRESS_REGEX }
-  validates :zipcode, presence: true, format: { with: VALID_ZIPCODE_REGEX }
-  validates :country, presence: true, format: { with: VALID_ADDRESS_REGEX }
+  validates :line1, presence: true
+  validates :city, presence: true
+  validates :zipcode, presence: true
+  validates :country, presence: true
 
 end
