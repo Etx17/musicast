c4a8f30d1df8fd0a0662204336683dc122a7195c
Etienne de Dianous <etiennededi@hotmail.fr>
2025-03-11 19:10:49 +0100

    ok

--- app/models/choice_imposed_work.rb
+++ app/models/choice_imposed_work.rb
@@ -7,5 +7,4 @@ class ChoiceImposedWork < ApplicationRecord
   accepts_nested_attributes_for :choice_imposed_work_airs, allow_destroy: true
   accepts_nested_attributes_for :airs, allow_destroy: true
   belongs_to :category
-
 end
