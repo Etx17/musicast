41c7b67a2a4e95d97525cf2a730b43affd399c6d
Etienne de Dianous <etiennededi@hotmail.fr>
2025-04-16 13:46:52 +0200

    ok

--- app/models/education.rb
+++ app/models/education.rb
@@ -1,5 +1,6 @@
 class Education < ApplicationRecord
   belongs_to :candidat
+  has_one_attached :logo, dependent: :destroy
   validates :title, :description, presence: true
   validates :title, length: { minimum: 2, maximum: 30 }
   validates :description, length: { minimum: 2, maximum: 300 }
