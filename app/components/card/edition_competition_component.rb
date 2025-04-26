30f8174ea8e37aa6707a68a4e03777613a9075be
Etienne de Dianous <etiennededi@hotmail.fr>
2025-04-26 15:38:24 +0200

    ok

--- app/components/card/edition_competition_component.rb
+++ app/components/card/edition_competition_component.rb
@@ -16,4 +16,27 @@ class Card::EditionCompetitionComponent < ViewComponent::Base
     @image_style = image_style
     @highlight_card = highlight_card
   end
+
+  def prizes_count_bottom_left_label
+    prizes_count = 0
+    @edition_competition.categories.each do |c|
+      prizes_count += c.prizes.count
+    end
+
+    if prizes_count == 0
+      {
+        icon: "fi fi-rs-trophy",
+        class: "text-primary h4 mb-0",
+        label: "#{prizes_count} #{t('global.prize')}"
+      }
+    elsif prizes_count > 1
+    {
+      icon: "fi fi-rs-trophy",
+      class: "text-primary h4 mb-0",
+      label: "#{prizes_count} #{t('global.prizes')}"
+    }
+    else
+      nil
+    end
+  end
 end
