87cde928fd9f43f232dacb33eff03f689536147c
Etienne de Dianous <etiennededi@hotmail.fr>
2025-05-07 11:17:27 +0200

    wip

--- app/components/search_filter_component.rb
+++ app/components/search_filter_component.rb
@@ -2,34 +2,19 @@ class SearchFilterComponent < ViewComponent::Base
   def initialize(search_path:, client:, clear_path:, show_filter_mobile: true, filters_options: [], buttons: [], filter_id: nil, settings_path: nil, remote: false, format: "html", show_search_bar: true, params: {})
     super()
     @search_path = search_path
-    @filters_options = filters_options # has a specific structure
-    @buttons = buttons # has a specific structure too (see above)
+    @filters_options = filters_options
+    @buttons = buttons
     @client = client
     @clear_path = clear_path
     @settings_path = settings_path
-    @remote = remote # for the form with ajax call
-    @format = format # for the form that need format: js attribute, (invitees attendance tab for example)
+    @remote = remote
+    @format = format
     @show_search_bar = show_search_bar
     @filter_id = filter_id
     @show_filter_mobile = show_filter_mobile
     @hidden_params = params
   end
 
-  # Originally method from application.html.erb, copied here for better encapsulation (rather that including Application Helper)
-  def translate_for_client(name_space, item, locals = {})
-    if name_space.present?
-      if item[0] == "."
-        item = caller_locations(1..1).first.path.split(".").first.gsub(Rails.root.to_s + "/app/views", "").split("/").join(".").gsub("._", ".") + item
-      end
-      item_locals = locals
-      item_locals[:default] = I18n.translate(item, **locals).html_safe
-      item_locals[:scope] = name_space
-      item_locals[:locale] = I18n.locale
-      I18n.translate(item, **item_locals).html_safe
-    else
-      I18n.translate(item, **locals).html_safe
-    end
-  end
 
   def filter_text
     params.dig(:filters, :text)
